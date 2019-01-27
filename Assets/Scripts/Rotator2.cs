14f53b3e6d23d43f5737de301c3e73a39b4cce1d
Kryptghoul <46969834+kryptghoul@users.noreply.github.com>
2019-01-27 17:33:40 -0500

    Files

---
+++ Assets/Scripts/Rotator2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator2 : MonoBehaviour{
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
