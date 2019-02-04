e309d802f0c306677ba104396643688ceaa31d93
Kay <46969834+kryptghoul@users.noreply.github.com>
2019-02-03 22:59:32 -0500

    Add files via upload

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -2,34 +2,40 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerController2 : MonoBehaviour{
+public class PlayerController2 : MonoBehaviour {
 
     public float speed;
     public Text countText;
     public Text winText;
+    public Text loseText;
+    public Text livesText;
 
     private Rigidbody rb;
     private int count;
+    private int lives;
 
-    void Start ()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
-        SetCountText();
+        SetCountText ();
+        lives = 3;
+        SetLivesText ();
         winText.text = "";
+        loseText.text = "";
     }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         if (Input.GetKey("escape"))
             Application.Quit();
 
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce (movement * speed);
+        rb.AddForce(movement * speed);
     }
 
     void OnTriggerEnter(Collider other)
@@ -40,14 +46,39 @@ public class PlayerController2 : MonoBehaviour{
             count = count + 1;
             SetCountText ();
         }
+
+        if (other.gameObject.CompareTag("Enemy Pickup"))
+        {
+            other.gameObject.SetActive (false);
+            lives = lives - 1;
+            SetLivesText ();
+            if (0 >= lives)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        if (count == 12)
+        {
+        transform.position = new Vector3(-60.0f, transform.position.y, transform.position.z); 
+        }
     }
 
-    void SetCountText ()
+    void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 20)
         {
             winText.text = "You Win!";
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
+        if (0 >= lives)
+        {
+            loseText.text = "You Lose!";
         }
     }
 }
--- Assets/Scripts/Rotator2.cs
+++ Assets/Scripts/Rotator2.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator2 : MonoBehaviour{
+public class Rotator2 : MonoBehaviour
+{
 
     // Start is called before the first frame update
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(45, 30, 15) * Time.deltaTime);
     }
 }
